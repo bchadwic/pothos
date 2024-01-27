45c4351a9f0e4816781e0a4818f68b996b40fe86
bchadwic <benchadwick87@gmail.com>
2024-01-27 01:24:55 -0800

    wip, added some styling and what not

--- main.go
+++ main.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"net/http"
-	"os"
 )
 
 func main() {
-	handler := &Handler{}
-	if err := http.ListenAndServe(":8080", handler); err != nil {
+	http.Handle("/", http.FileServer(http.Dir("./resources/")))
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
-}
-
-type Handler struct{}
-
-func (handler *Handler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-	body, err := os.ReadFile("./resources/index.html")
-	if err != nil {
-		panic(err)
-	}
-	resp.Write(body)
 }
