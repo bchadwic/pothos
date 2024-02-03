28220da32c06a9b2d2d3085b858a8460c03ef74a
bchadwic <benchadwick87@gmail.com>
2024-02-02 21:03:37 -0800

    test

--- main.go
+++ main.go
@@ -1,12 +1,48 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
+type Post struct {
+	Title       string
+	Description string
+}
+
+// TodoPageData list of page
+type TodoPageData struct {
+	Posts []Post
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./resources/")))
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
+	css := http.FileServer(http.Dir("./resources/stylesheets"))
+	http.Handle("/stylesheets/", http.StripPrefix("/stylesheets", css))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("./resources/pages/index.html"))
+		data := TodoPageData{
+			Posts: []Post{
+				{Title: "Task 1 is here", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique, nesciunt autem corrupti laborum, perferendis eos nemo corporis ducimus velit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique"},
+				{Title: "Task 2", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique"},
+				{Title: "Task 1 is here", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique, nesciunt autem corrupti laborum, perferendis eos nemo corporis ducimus velit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique"},
+				{Title: "Task 2", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique"},
+				{Title: "Task 1 is here", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique, nesciunt autem corrupti laborum, perferendis eos nemo corporis ducimus velit!"},
+				{Title: "Task 2", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique"},
+				{Title: "Task 1 is here", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique, nesciunt autem corrupti laborum, perferendis eos nemo corporis ducimus velit!"},
+				{Title: "Task 2", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique"},
+				{Title: "Task 1 is here", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique, nesciunt autem corrupti laborum, perferendis eos nemo corporis ducimus velit!"},
+				{Title: "Task 2", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque repudiandae ut maxime tenetur id non iste libero consequatur similique"},
+			},
+		}
+		tmpl.Execute(w, data)
+	})
+
+	fmt.Println("Listening on localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
+
 }
